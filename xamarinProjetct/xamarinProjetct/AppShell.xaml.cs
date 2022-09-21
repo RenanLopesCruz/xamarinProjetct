3ee1d51745337c87ba7a0638c59888382446ec44
EnzoSylvestrin <enzospavani@gmail.com>
2022-09-21 07:19:02 -0300

    Add project files.

---
+++ xamarinProjetct/xamarinProjetct/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+using xamarinProjetct.ViewModels;
+using xamarinProjetct.Views;
+
+namespace xamarinProjetct
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
---
+++ xamarinProjetct/xamarinProjetct/Views/NewItemPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using xamarinProjetct.Models;
+using xamarinProjetct.ViewModels;
+
+namespace xamarinProjetct.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
